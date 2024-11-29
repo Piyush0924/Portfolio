e87de67c3e144247dc38e1f7ff412ba54042d250
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2024-11-29 22:20:44 +0530

    px-8 skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -91,7 +91,7 @@ const Skills = () => {
           </p>
         </div>
 
-        <div className="w-full h-[100vh] mt-4 grid grid-cols-2 sm:grid-cols-3 gap-12 text-center px-4 py-8 sm:px-0">
+        <div className="w-full h-[100vh] mt-4 grid grid-cols-2 sm:grid-cols-3 gap-12 text-center px-8 py-12 sm:px-0">
           {skill.map(({ id, src, title, style }) => (
             <div
               key={id}
