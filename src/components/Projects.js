60ca4d3066a4a17cb5e7aa2dc15010de5a886693
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2024-11-29 21:36:44 +0530

    putting the project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,35 @@ import React, { useState } from "react";
 const projectData = [
   {
     id: 1,
+    // 
+    src:"/cryptofrontdashboard.jpg",
+    l: "https://github.com/Piyush0924/cryptofrontdashboard",
+    href: "https://cryptofrontdashboard.vercel.app/",
+    details: "This is a Crypto currency dashboard project that shows price charts, calculates exchange rates, and displays a list of top cryptocurrencies according to their market cap. It is created with React and Redux JavaScript libraries, styled components, and utilizes CoinGecko's API for information.",
+  },
+  {
+    id: 2,
     // src: bookmyshow,
     src:"/bookmyshow.jpg",
-    l: "https://github.com/sumit-nagrikar/cinebuzz.git",
+    l: "https://github.com/sumit-nagrikar/cinebuzz",
     href: "https://cinebuzzz.netlify.app/",
     details: "Our project, CineBuzz, addresses these issues by providing a modern, user-friendly platform where users can easily browse movies, check showtimes, and book tickets online.",
-  },
+  }, {
+    id: 3,
+    // the entertainment app
+    src:"/TheEntertainmentApp.jpg",
+    l: "https://github.com/Piyush0924/The-Entertainment-App",
+    href: "https://mirajshow.netlify.app/",
+  
+    details: "The Entertainment App is designed for movie and TV lovers to easily find and save their favorite content. **Why is it useful?** It helps users quickly discover new shows and movies, streamlining their search for great entertainment. **How is it useful?** With an easy-to-use interface, it allows users to bookmark their favorites and get personalized recommendations, making their viewing experience smoother and more enjoyable"
+  }, 
+  // {
+  //   id: 4,
+  //   src:"/E-commerce",
+  //   l: "https://github.com/sumit-nagrikar/cinebuzz.git",
+  //   href: "https://cinebuzzz.netlify.app/",
+  //   details: "Our project, CineBuzz, addresses these issues by providing a modern, user-friendly platform where users can easily browse movies, check showtimes, and book tickets online.",
+  // },
  
 ];
 
